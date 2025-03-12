07e2a46287f10d9fb0505277ed3f9c16cbf794d8
Taylor Jenkins <tjenkins@udel.edu>
2025-03-11 21:32:02 -0400

    solved everything

--- src/bad-components/DoubleHalf.tsx
+++ src/bad-components/DoubleHalf.tsx
@@ -1,40 +1,71 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
-import { dhValue, setDhValue } from "./DoubleHalfState";
-
-function Doubler(): React.JSX.Element {
-    return (
-        <Button
-            onClick={() => {
-                setDhValue(2 * dhValue);
-            }}
-        >
-            Double
-        </Button>
-    );
-}
-
-function Halver(): React.JSX.Element {
-    return (
-        <Button
-            onClick={() => {
-                setDhValue(0.5 * dhValue);
-            }}
-        >
-            Halve
-        </Button>
-    );
-}
+//import { dhValue, setDhValue } from "./DoubleHalfState";
 
 export function DoubleHalf(): React.JSX.Element {
+    let [dhValue, setDhValue] = useState<number>(10);
     return (
         <div>
             <h3>Double Half</h3>
             <div>
                 The current value is: <span>{dhValue}</span>
             </div>
-            <Doubler></Doubler>
-            <Halver></Halver>
+            <Button
+                onClick={() => {
+                    setDhValue(2 * dhValue);
+                }}
+            >
+                Double
+            </Button>
+            <Button
+                onClick={() => {
+                    setDhValue(0.5 * dhValue);
+                }}
+            >
+                Halve
+            </Button>
+            {/* <Doubler></Doubler>
+            <Halver></Halver> */}
         </div>
     );
 }
+
+// function Doubler(): React.JSX.Element {
+//     let [dhValue, setDhValue] = useState<number>(10);
+//     return (
+//         <Button
+//             onClick={() => {
+//                 setDhValue(2 * dhValue);
+//             }}
+//         >
+//             Double
+//         </Button>
+//     );
+// }
+
+// function Halver(): React.JSX.Element {
+//     let [dhValue, setDhValue] = useState<number>(10);
+//     return (
+//         <Button
+//             onClick={() => {
+//                 setDhValue(0.5 * dhValue);
+//             }}
+//         >
+//             Halve
+//         </Button>
+//     );
+// }
+
+// export function DoubleHalf(): React.JSX.Element {
+//     let [dhValue, setDhValue] = useState<number>(10);
+//     return (
+//         <div>
+//             <h3>Double Half</h3>
+//             <div>
+//                 The current value is: <span>{dhValue}</span>
+//             </div>
+//             <Doubler></Doubler>
+//             <Halver></Halver>
+//         </div>
+//     );
+// }
--- src/nested.ts
+++ src/nested.ts
@@ -1,12 +1,14 @@
+//import { queryByTestId } from "@testing-library/dom";
 import { Answer } from "./interfaces/answer";
 import { Question, QuestionType } from "./interfaces/question";
+import { duplicateQuestion, makeBlankQuestion } from "./objects";
 
 /**
  * Consumes an array of questions and returns a new array with only the questions
  * that are `published`.
  */
 export function getPublishedQuestions(questions: Question[]): Question[] {
-    return [];
+    return questions.filter((question) => question.published);
 }
 
 /**
@@ -15,7 +17,15 @@ export function getPublishedQuestions(questions: Question[]): Question[] {
  * `expected`, and an empty array for its `options`.
  */
 export function getNonEmptyQuestions(questions: Question[]): Question[] {
-    return [];
+    let newQuestions: Question[] = [...questions];
+    return newQuestions.filter(
+        (question) =>
+            !(
+                question.body === "" &&
+                question.expected === "" &&
+                question.options.length === 0
+            ),
+    );
 }
 
 /***
@@ -24,9 +34,12 @@ export function getNonEmptyQuestions(questions: Question[]): Question[] {
  */
 export function findQuestion(
     questions: Question[],
-    id: number
+    id: number,
 ): Question | null {
-    return null;
+    let chosenQuestion = questions.find(
+        (question: Question) => question.id === id,
+    );
+    return chosenQuestion || null;
 }
 
 /**
@@ -34,7 +47,10 @@ export function findQuestion(
  * with the given `id`.
  */
 export function removeQuestion(questions: Question[], id: number): Question[] {
-    return [];
+    let newQuestions = questions.filter(
+        (question: Question) => question.id !== id,
+    );
+    return newQuestions;
 }
 
 /***
@@ -42,21 +58,35 @@ export function removeQuestion(questions: Question[], id: number): Question[] {
  * questions, as an array.
  */
 export function getNames(questions: Question[]): string[] {
-    return [];
+    let names = questions.map((question: Question): string => question.name);
+    return names;
 }
 
 /***
  * Consumes an array of questions and returns the sum total of all their points added together.
  */
 export function sumPoints(questions: Question[]): number {
-    return 0;
+    let totalPoints = questions.reduce(
+        (currentSum: number, question: Question) =>
+            currentSum + question.points,
+        0,
+    );
+    return totalPoints;
 }
 
 /***
  * Consumes an array of questions and returns the sum total of the PUBLISHED questions.
  */
 export function sumPublishedPoints(questions: Question[]): number {
-    return 0;
+    let publishedQuestions = questions.filter(
+        (question: Question) => question.published,
+    );
+    let sumTotal = publishedQuestions.reduce(
+        (currentSum: number, question: Question) =>
+            currentSum + question.points,
+        0,
+    );
+    return sumTotal;
 }
 
 /***
@@ -77,7 +107,13 @@ id,name,options,points,published
  * Check the unit tests for more examples!
  */
 export function toCSV(questions: Question[]): string {
-    return "";
+    let questionCSV = questions
+        .map(
+            (question: Question): string =>
+                `${question.id},${question.name},${question.options.length},${question.points},${question.published}`,
+        )
+        .join("\n");
+    return "id,name,options,points,published\n" + questionCSV;
 }
 
 /**
@@ -86,7 +122,15 @@ export function toCSV(questions: Question[]): string {
  * making the `text` an empty string, and using false for both `submitted` and `correct`.
  */
 export function makeAnswers(questions: Question[]): Answer[] {
-    return [];
+    let Answers = questions.map(
+        (question: Question): Answer => ({
+            questionId: question.id,
+            text: "",
+            submitted: false,
+            correct: false,
+        }),
+    );
+    return Answers;
 }
 
 /***
@@ -94,7 +138,9 @@ export function makeAnswers(questions: Question[]): Answer[] {
  * each question is now published, regardless of its previous published status.
  */
 export function publishAll(questions: Question[]): Question[] {
-    return [];
+    return questions.map(
+        (question: Question): Question => ({ ...question, published: true }),
+    );
 }
 
 /***
@@ -102,7 +148,11 @@ export function publishAll(questions: Question[]): Question[] {
  * are the same type. They can be any type, as long as they are all the SAME type.
  */
 export function sameType(questions: Question[]): boolean {
-    return false;
+    if (questions.length === 0) {
+        return true;
+    }
+    let type: QuestionType = questions[0].type;
+    return questions.every((question) => question.type === type);
 }
 
 /***
@@ -114,9 +164,10 @@ export function addNewQuestion(
     questions: Question[],
     id: number,
     name: string,
-    type: QuestionType
+    type: QuestionType,
 ): Question[] {
-    return [];
+    let newList = [...questions, makeBlankQuestion(id, name, type)];
+    return newList;
 }
 
 /***
@@ -127,9 +178,15 @@ export function addNewQuestion(
 export function renameQuestionById(
     questions: Question[],
     targetId: number,
-    newName: string
+    newName: string,
 ): Question[] {
-    return [];
+    let target = questions.map(
+        (question: Question): Question => ({
+            ...question,
+            name: question.id === targetId ? newName : question.name,
+        }),
+    );
+    return target;
 }
 
 /***
@@ -142,9 +199,23 @@ export function renameQuestionById(
 export function changeQuestionTypeById(
     questions: Question[],
     targetId: number,
-    newQuestionType: QuestionType
+    newQuestionType: QuestionType,
 ): Question[] {
-    return [];
+    let target = questions.map(
+        (question: Question): Question => ({
+            ...question,
+            type: question.id === targetId ? newQuestionType : question.type,
+            options:
+                (
+                    question.id === targetId &&
+                    newQuestionType !== "multiple_choice_question"
+                ) ?
+                    []
+                :   question.options,
+        }),
+    );
+    return target;
+    //return [];
 }
 
 /**
@@ -161,9 +232,23 @@ export function editOption(
     questions: Question[],
     targetId: number,
     targetOptionIndex: number,
-    newOption: string
-) {
-    return [];
+    newOption: string,
+): Question[] {
+    return questions.map((question: Question): Question => {
+        if (question.id === targetId) {
+            if (targetOptionIndex === -1) {
+                return {
+                    ...question,
+                    options: [...question.options, newOption],
+                };
+            } else {
+                let newList = [...question.options];
+                newList[targetOptionIndex] = newOption;
+                return { ...question, options: newList };
+            }
+        }
+        return question;
+    });
 }
 
 /***
@@ -175,7 +260,12 @@ export function editOption(
 export function duplicateQuestionInArray(
     questions: Question[],
     targetId: number,
-    newId: number
+    newId: number,
 ): Question[] {
-    return [];
+    let newList = questions.flatMap((question: Question) =>
+        question.id === targetId ?
+            [question, duplicateQuestion(newId, question)]
+        :   [question],
+    );
+    return newList;
 }
